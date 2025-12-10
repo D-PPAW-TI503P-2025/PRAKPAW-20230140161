042cb71e1489297ef56de77a577f8f889b65ea00
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-12-10 21:31:49 +0700

    ,

--- 20230140161-server/routes/reports.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken, isAdmin } = require('../middleware/permissionMiddleware');
-const reportController = require('../controllers/reportController');
-
-
-router.get('/daily', [authenticateToken, isAdmin], reportController.getDailyReport);
-
-module.exports = router;
